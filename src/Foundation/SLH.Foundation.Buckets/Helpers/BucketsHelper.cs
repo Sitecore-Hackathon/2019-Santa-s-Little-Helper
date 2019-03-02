0fc4242deeb8afc04a22ae55cd3c9b87d8cafd22
HQ\Santiago.Morla <santiago.morla@verndale.com>
2019-03-02 14:54:22 -0500

    Added ItemHelper to get field values

--- src/Foundation/SLH.Foundation.Buckets/Helpers/BucketsHelper.cs
+++ src/Foundation/SLH.Foundation.Buckets/Helpers/BucketsHelper.cs
@@ -6,6 +6,7 @@ using Sitecore.Data.Managers;
 using SLH.Foundation.Buckets.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Sitecore.Data.Fields;
 using Sitecore.Shell.Applications.ContentEditor;
 
 namespace SLH.Foundation.Buckets.Helpers
@@ -57,10 +58,79 @@ namespace SLH.Foundation.Buckets.Helpers
             var fields = inheritTemplates.SelectMany(x => x.GetFields(false)).Distinct();
             foreach (var field in fields)
             {
-                templateFields.Add(new TemplateField { FieldName = field.Name, FieldValue = item[field.ID] });
+                templateFields.Add(new TemplateField { FieldName = field.Name, FieldValue = GetFieldValue(field, item) });
             }
 
             return templateFields;
+        }
+
+        public static string GetFieldValue(Sitecore.Data.Templates.TemplateField field, Item item)
+        {
+            switch (field.Type.ToLower())
+            {
+                case "tristate":
+                    return "TriState";
+                case "checkbox":
+                    return item.GetCheckBoxValueByFieldName(field.Name).ToString();
+
+                case "date":
+                case "datetime":
+                    return item.Fields[field.Name].Value;
+
+                case "number":
+                    return item.Fields[field.Name].Value;
+
+                case "integer":
+                    return item.Fields[field.Name].Value;
+
+                case "treelist with search":
+                case "treelist":
+                case "treelistex":
+                case "treelist descriptive":
+                case "checklist":
+                case "multilist with search":
+                case "multilist":
+                    var items = item[field.Name].GetMultiListParameterItems();
+                    return string.Join(", ", items.Select(x => x.DisplayName));
+
+                case "grouped droplink":
+                case "droplink":
+                case "lookup":
+                case "droptree":
+                case "reference":
+                case "tree":
+                    return item.GetPropertyAsItem(field.Name).DisplayName;
+
+                case "file":
+                    FileField fileField = item.Fields[field.Name];
+                    return fileField.MediaItem.DisplayName;
+
+                case "image":
+                    ImageField imgField = item.Fields[field.Name];
+                    return $"<img src={imgField.MediaItem.GetImageUrl()} alt={imgField.Alt} />"; 
+
+                case "general link":
+                case "general link with search":
+                case "custom general link":
+                    LinkField linkField = item.Fields[field.Name];
+                    return $"<a href={linkField.GetGeneralLinkUrl()} target={linkField.GetTarget()} >{linkField.Title}</a>";
+
+                case "password":
+                case "icon":
+                case "rich text":
+                case "html":
+                case "single-line text":
+                case "multi-line text":
+                case "frame":
+                case "text":
+                case "memo":
+                case "droplist":
+                case "grouped droplist":
+                case "valuelookup":
+                    return item.Fields[field.Name].Value;
+            }
+
+            return string.Empty;
         }
 
         public static string GenerateFieldEditorUrl(Item item, IEnumerable<TemplateField> fields)
---
+++ src/Foundation/SLH.Foundation.Buckets/Helpers/ItemHelper.cs
@@ -0,0 +1,135 @@
+﻿using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sitecore.Data.Fields;
+using Sitecore.Links;
+using Sitecore.Resources.Media;
+
+namespace SLH.Foundation.Buckets.Helpers
+{
+    public static class ItemHelper
+    {
+        public static IEnumerable<Item> GetMultiListParameterItems(this string itemIds)
+        {
+            if (string.IsNullOrEmpty(itemIds))
+                return null;
+
+            var splitItemList = itemIds.Split(new[] { "|" }, StringSplitOptions.RemoveEmptyEntries);
+
+            var itemList = splitItemList.Select(itemId =>
+            {
+                Item item = null;
+                Guid id = Guid.Empty;
+                if (Guid.TryParse(itemId, out id))
+                {
+                    item = Context.Database.GetItem(new ID(id));
+                }
+                return item;
+            });
+
+            return itemList.Where(f => f != null);
+        }
+
+        public static bool GetCheckBoxValueByFieldName(this Item item, string fieldName)
+        {
+            bool flag = false;
+
+            try
+            {
+                flag = item.Fields[fieldName].Value == "1";
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            return flag;
+        }
+
+        public static Item GetPropertyAsItem(this Item item, string propertyName)
+        {
+            if (item == null || string.IsNullOrEmpty(propertyName))
+                return null;
+
+            var ret = Context.Database.GetItem(item[propertyName]);
+
+            return ret;
+        }
+
+        public static string GetGeneralLinkUrl(this LinkField linkField)
+        {
+            string url = string.Empty;
+            if (linkField == null)
+                return string.Empty;
+
+            switch (linkField.LinkType)
+            {
+                case "internal":
+                    url = linkField.TargetItem != null ? GetItemUrl(linkField.TargetItem) : String.Empty;
+                    return url;
+                case "external":
+                case "mailto":
+                case "anchor":
+                case "javascript":
+                    url = linkField.Url;
+                    return url;
+                case "media":
+                    url = linkField.TargetItem != null
+                        ? MediaManager.GetMediaUrl((Item)new MediaItem(linkField.TargetItem))
+                        : String.Empty;
+                    return url;
+                default:
+                    return String.Empty;
+            }
+        }
+
+        public static string GetItemUrl(Item item)
+        {
+            if (item == null)
+                return string.Empty;
+            return LinkManager.GetItemUrl(item);
+        }
+
+        public static string GetTarget(this LinkField targetValue)
+        {
+            switch (targetValue.Target)
+            {
+                case "_blank":
+                case "blank":
+                case "New Browser":
+                    return "_blank";
+                default:
+                    return "";
+            }
+        }
+
+        public static string GetImageUrl(this Item currentItem, int maxWidth = 0, int width = 0, int maxHeight = 0)
+        {
+            if (currentItem == null)
+                return null;
+
+            var image = (MediaItem)currentItem;
+
+            // If there's options specified, add them to the options object.
+            if (width > 0 || maxWidth > 0 || maxHeight > 0)
+            {
+                var options = new MediaUrlOptions();
+                if (width > 0)
+                    options.Width = width;
+                if (maxWidth > 0)
+                    options.MaxWidth = maxWidth;
+                if (maxHeight > 0)
+                    options.MaxHeight = maxHeight;
+
+                return StringUtil.EnsurePrefix('/',
+                    HashingUtils.ProtectAssetUrl(MediaManager.GetMediaUrl(image, options)));
+            }
+
+            // Otherwise, get the image with no options specified.
+            return StringUtil.EnsurePrefix('/', MediaManager.GetMediaUrl(image));
+        }
+    }
+}
