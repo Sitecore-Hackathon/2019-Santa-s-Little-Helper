1b1579bc3c3c15d7c2a235dd60228ccea6a980b4
Juan Arias <juanarias@rpa.com>
2019-03-02 00:51:48 -0800

    Add sitecore application and menu.

---
+++ src/Project/Website/sitecore modules/Shell/SLH/SLH.js
@@ -0,0 +1,14 @@
+﻿
+$(document).ready(function () {
+    $("#article-table").tabulator({
+        columns: [
+            { title: "", field: "cheese", sorter: "boolean", align: "center", formatter: "tickCross" },
+            { title: "Title", field: "name", sorter: "string", width: 200, editor: true },
+            { title: "Source", field: "age", sorter: "number", align: "right", formatter: "progress" },
+            { title: "Categories", field: "gender", sorter: "string", cellClick: function (e, cell) { console.log("cell click") }, },
+            { title: "Date", field: "height", formatter: "star", align: "center", width: 100 },
+            { title: "Published Date", field: "col", sorter: "string" },
+            { title: "Actions", field: "dob", sorter: "date", align: "center" }
+        ],
+    });
+});
